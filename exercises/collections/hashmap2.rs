ad737a3218fbacfdf7227c2e623d01f7428f2b50
Jan <janvanbuel@hotmail.com>
2021-09-23 20:58:15 +0200

    collections exercises

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -12,8 +12,6 @@
 // Execute the command `rustlings hint hashmap2` if you need
 // hints.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -35,9 +33,12 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Put new fruits if not already present. Note that you
-        // are not allowed to put any type of fruit that's already
-        // present!
+        match fruit {
+            Fruit::Apple | Fruit::Mango | Fruit::Lychee => (),
+            x => {
+                basket.insert(x, 2);
+            }
+        }
     }
 }
 
